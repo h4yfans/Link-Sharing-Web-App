015c8e6846383749b3add4337f5cbb6f537f4b67
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-27 11:31:53 +0300

    Blade template fix In blade pages added @extends tag and pages properly formetted

---
+++ resources/views/feeds/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+<div class="container">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <span class="col-md-1 col-md-1 glyphicon glyphicon-chevron-up">
+                <!-- TODO// if feed is upvoted or downvoted, add class 'faded'  -->
+            </span>
+            <div class="col-md-11"><a href="">Link title here Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio
+                dolorem magnam molestias quis quisqudio officia, quidem rem suscipit.</a>
+            </div>
+            <span class="col-md-1 col-md-1 glyphicon glyphicon-chevron-down">
+                <!-- TODO// if feed is upvoted or downvoted, add class 'faded'  -->
+            </span>
+            <div class="author pull-right">
+                <strong><a href="">h4yfans</a> - 2 minutes ago</strong>
+            </div>
+        </div>
+    </div>
+</div>
