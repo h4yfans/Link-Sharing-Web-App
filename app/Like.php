3b1d9effe0ab749bb6c6b25534eeb18d583844f5
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-27 13:45:19 +0300

    Set relationship between models - Like model has a relationship with User and Post - User model has a relationship with Post and Like - Post model has a relationship with User and Like

--- app/Like.php
+++ app/Like.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function post(){
+        return $this->belongsTo('App\Post');
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function likes(){
+        return $this->hasMany('App\Like');
+    }
 }
