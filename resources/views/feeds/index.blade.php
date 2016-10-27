4d4e5e784ac1d2701882caccf3e784a5b13f39ce
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-27 11:49:38 +0300

    blade template files fix - login, register, index files fixed. @extends and @section tags added into files.

--- resources/views/feeds/index.blade.php
+++ resources/views/feeds/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('content')
 <div class="container">
     <div class="panel panel-default">
         <div class="panel-body">
@@ -18,3 +19,5 @@
         </div>
     </div>
 </div>
+
+    @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,5 +86,7 @@
 
 <!-- Scripts -->
 <script src="/js/app.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
