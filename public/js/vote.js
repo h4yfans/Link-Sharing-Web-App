dbc37865fcc70484c3865a06529e02209f07c983
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-30 00:25:05 +0300

    fetch data with ajax - fetch vote status data and display -change glyphicon's size

--- public/js/vote.js
+++ public/js/vote.js
@@ -8,24 +8,40 @@ $(document).ready(function () {
 
     //handle
     $('.vote').click(function (e) {
-        // e.preventDefault();
-        if (AuthCheck) {
-            postID = e.target.parentNode.parentNode.parentNode.parentNode.dataset['postid'];
-            console.log(postID);
+        e.preventDefault();
 
+        if (AuthCheck) {
+            //don't want to use jquery. idk why but i like parentnode :)
+            postID = e.target.parentNode.parentNode.parentNode.parentNode.dataset['postid'];
+            // change links like status
             $.ajax({
-                url: 'vote',
+                url: 'UpdateLike',
                 type: 'POST',
                 data: {
                     class: $(this).attr('class'),
                     postId: postID
+                }
+            });
+            // display sum of votes
+            $.ajax({
+                url: 'UpdateLikeSum',
+                type: 'POST',
+                data: {
+                    // class: $(this).attr('class'),
+                    postId: postID
                 },
-                success: function (data) {
-                    console.log(data);
+                success: function( voteStatus ) {
+                    $('.votes-' + postID).text(voteStatus.post + ' votes so far');
                 }
             });
 
+            /*var voted = $(this).hasClass('active');
+            if(voted){
+                $(this).removeClass('active')
 
+            }else if(!voted){
+                $(this).addClass('active')
+            }*/
 
         } else {
             $('#myModal').modal('show');
